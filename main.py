4c448eea46a48778cc74781c41981e25c883aeaf
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-10-14 17:06:06 +0200

    start parsing args

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from enum import Enum
+import argparse
+import sys
+
+
+class TensionRating(Enum):
+    Low = 1
+    Medium = 2
+    High = 3
 
 
 class String:
@@ -24,6 +32,53 @@ class String:
         self.material = material
         self.gauge = gauge
 
+    def fromArgs(args):
+        material = args.material.lower()
+        if material == "nylon":
+            material = String.Material.Nylon
+        elif material == "steel":
+            material = String.Material.Steel
+        elif material == "bronze":
+            material = String.Material.Bronze
+        else:
+            raise ValueError(f"Invalid string material: {material}")
 
-scaleLength = 123
-lowTensionNylon = String(scaleLength, String.Material.Nylon, 0.001)
+        tension = args.tension.lower()
+        if tension == "low":
+            tension = TensionRating.Low
+        elif tension == "medium":
+            tension = TensionRating.Medium
+        elif tension == "high":
+            tension = TensionRating.High
+        else:
+            raise ValueError(f"Invalid tension: {tension}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Plink plonk")
+
+    parser.add_argument(
+        "--material",
+        "-m",
+        help="Material, can be Nylon, Steel or Bronze",
+        required=True,
+    )
+    parser.add_argument(
+        "--tension",
+        "-t",
+        help="String tension rating, can be Low, Medium or High",
+        default="Medium",
+    )
+
+    args = parser.parse_args()
+
+    try:
+        string = String.fromArgs(args)
+    except ValueError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+    print(string)
+
+
+if __name__ == "__main__":
+    main()
